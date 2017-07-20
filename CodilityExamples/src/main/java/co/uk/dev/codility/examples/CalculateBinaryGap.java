ebeb5163da19a3410a77e80de5d2af29f2072068
devjob2012 <devjob.2012@gmail.com>
2017-07-20 10:24:38 +0100

    Initial version

---
+++ CodilityExamples/src/main/java/co/uk/dev/codility/examples/CalculateBinaryGap.java
@@ -0,0 +1,39 @@
+package co.uk.dev.codility.examples;
+
+import static java.lang.Integer.toBinaryString;
+
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class CalculateBinaryGap {
+	public static void main(String[] args) {
+		CalculateBinaryGap cal = new CalculateBinaryGap();
+		System.out.println(cal.calculateBinaryGap(37));
+
+	}
+
+	public int calculateBinaryGap(int N) {
+		return Stream
+				.of(
+						// integer to binary string
+						toBinaryString(N)
+								// trim 0(s) at the end
+								.replaceAll("0+$", "")
+								// split string with 1(s)
+								.split("1+"))
+				// lambda expressions: use filter to keep not null elements
+				.filter(a -> a != null)
+				// method references: convert string to integer by using the
+				// length of string
+				.map(String::length)
+				// method references: find the largest number in the stream by
+				// using integer comparator
+				.max(Integer::compare)
+				// return 0 if nothing matches after the process
+				.orElse(0);
+	}
+}
+// http://www.oracle.com/technetwork/articles/java/ma14-java-se-8-streams-2177646.html
